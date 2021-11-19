cfade1d4b7c0e3cf6c9b67e71ab40da2e69cf189
LA-Miller <lamiller516@gmail.com>
2021-11-18 22:05:02 -0500

    file changes

--- team-git/src/App.js
+++ team-git/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import UseGeoLocation from './geolocation/UseGeoLocation';
 
 function App() {
+  const location =  UseGeoLocation();
   
   return (
     <div className="App">
-      test
+      {
+        location.loaded ? JSON.stringify(location) : "Location data not available"
+      }
     </div>
   );
 }
