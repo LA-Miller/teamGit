e092955e818197843a7cd6902270abcb5704b24f
LA-Miller <lamiller516@gmail.com>
2021-11-22 19:52:55 -0500

    navbar logo showing up

--- team-git/src/App.js
+++ team-git/src/App.js
@@ -1,33 +1,38 @@
-
 import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import { Container, Row, Col } from "reactstrap";
 import GetEvents from "./apis/Events";
 import Weather from "./apis/Weather";
-import Footer from './site/Footer';
-import Headers from './site/Header';
-import Nasa from './apis/Nasa'
+import Footer from "./site/Footer";
+import Headers from "./site/Header";
+import Nasa from "./apis/Nasa";
 
 const App = () => {
-
   return (
-    <Container className="form">
-      <Row>
-        <Col className="weather" md="6" >
-        <Weather />
-        </Col>
-        <Col className="nasa" md="6">
-          <h2><Nasa lat={lat} lon={lon} /></h2>
-        </Col>
-      </Row>
-      <Row>
-        <Col className="events" md="12">
-          <h1 className="events-nearby">Events Near You!</h1>
-        <GetEvents />
-        </Col>
-      </Row>
-    </Container>    
+    <div className="page">
+      <div className="navbar">
+      <Headers />
+      </div>
+      <Container className="form">
+        <Row>
+          <Col className="weather" md="6">
+            <Weather />
+          </Col>
+          <Col className="nasa" md="6">
+            <h2>
+              <Nasa />
+            </h2>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="events" md="12">
+            <h1 className="events-nearby">Events Near You!</h1>
+            <GetEvents />
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
--- team-git/src/site/Header.js
+++ team-git/src/site/Header.js
@@ -1,29 +1,28 @@
-import {
-    Navbar,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink
-} from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+import logo from '../assets/logo.svg';
+
 
 const Header = () => {
-    return(
+  return (
     <header>
-        <Navbar style={{backgroundColor: '#C6ECF6'}} className='header'>
-            <NavbarBrand href='/'></NavbarBrand>
-            <img src="/logo.svg"
-            width="307"
-            height="154"
+      <Navbar className="header">
+        <NavbarBrand href="/">
+          <img
+            src={logo}
+            width="307px"
+            height="154px"
             className="d-inline-block align-top"
             alt="Your Locator Logo"
-            />
-            <Nav className='ml-auto' navbar>
-                <NavLink href='https://github.com/LA-Miller/teamGit/tree/master'>GitHub</NavLink>
-            </Nav>
-        </Navbar>
+          />
+        </NavbarBrand>
+        <Nav>
+          <NavLink href="https://github.com/LA-Miller/teamGit/tree/master">
+            GitHub
+          </NavLink>
+        </Nav>
+      </Navbar>
     </header>
-    
-    );
+  );
 };
 
 export default Header;
